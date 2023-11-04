132b90a388009a77d8bbabf690078187ce1d4dbd
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2023-11-04 11:33:55 +0100

    further simplications...

--- modules/api/src/main/scala/types.scala
+++ modules/api/src/main/scala/types.scala
@@ -1,24 +1,29 @@
-package common
-
+import cats.{Order, Show}
 import cats.syntax.all.*
-import cats.Order
-import cats.Show
-import common.circe.codecs.given
-import common.tapir.syntax.*
-import io.circe.Codec as CirceCodec
+import io.circe.{Decoder, Encoder, Codec as CirceCodec}
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.*
 import sttp.tapir
-import sttp.tapir.codec.iron.TapirCodecIron
 import sttp.tapir.Codec.PlainCodec as TapirPlainCodec
-import sttp.tapir.Codec as TapirCodec
-import sttp.tapir.Schema as TapirSchema
+import sttp.tapir.{CodecFormat, DecodeResult, Codec as TapirCodec, Schema as TapirSchema}
+import sttp.tapir.codec.iron.TapirCodecIron
+
+given CirceCodec[Int] = CirceCodec.from(Decoder.decodeInt, Encoder.encodeInt)
+
+extension[L, H: Show, CF <: CodecFormat] (codec: TapirCodec[L, H, CF])
+  def iemap[HH](f: H => Either[String, HH])(g: HH => H): TapirCodec[L, HH, CF] =
+    codec.mapDecode(s => DecodeResult.fromEitherString(s.show, f(s)))(g)
 
 private type VersionConstraints = DescribedAs[GreaterEqual[0], "Version must be positive"]
 opaque type Version = Int :| VersionConstraints
+
 object Version extends RefinedTypeOps[Int, VersionConstraints, Version], TapirCodecIron:
   given CirceCodec[Version] = summon[CirceCodec[Int]].iemap(either(_))(_.value)
+
   given TapirPlainCodec[Version] = summon[TapirPlainCodec[Int]].iemap(either(_))(_.value)
+
   given TapirSchema[Version] = ironTypeSchema[Int, VersionConstraints]
+
   given Order[Version] = Order.fromOrdering
+
   given Show[Version] = summon[Show[Int]].contramap(_.value)
